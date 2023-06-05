8f24e7674f5e2076551da30f940f3a520122a48e
aabapat2 <aabapat2@illinois.edu>
2023-06-04 21:37:32 -0400

    completed

--- Downloads/jovian-careers-website-main/app.py
+++ Downloads/jovian-careers-website-main/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from sqlalchemy import create_engine, text
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -14,7 +14,24 @@ def hello_jovian():
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs=load_jobs_from_db()
+  return jsonify(jobs)
+
+@app.route("/job/<id>")
+def showJob(id):
+  job=load_job_from_db(id)
+  if not job:
+     return "Not Found", 404
+  return render_template('jobpage.html', job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html', 
+                         application=data,
+                         job=job)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5001)#debug=True)
+  app.run(host='0.0.0.0', port=5001, debug=True)
